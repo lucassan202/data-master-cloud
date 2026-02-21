a1c7f32072f9c5dab67f59bc4a44825d3204be5b
Besgam <lucas.sa20@gmail.com>
2026-02-21 00:01:21 -0300

    teste

--- IaC/notebooks.tf
+++ IaC/notebooks.tf
@@ -12,7 +12,7 @@ variable "notebook_language" {
 }
 
 resource "databricks_notebook" "bronze_notebook" {
-  path     = "/lucas_san20@hotmail.com/${var.notebook_subdirectory}/bronze.py"
+  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/bronze.py"
   language = var.notebook_language
   source   = "../app/src/bronze.py"
 }
@@ -22,31 +22,31 @@ output "notebook_url" {
 }
 
 resource "databricks_notebook" "silver_notebook" {
-  path     = "/lucas_san20@hotmail.com/${var.notebook_subdirectory}/silver.py"
+  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/silver.py"
   language = var.notebook_language
   source   = "../app/src/silver.py"
 }
 
 resource "databricks_notebook" "problema_gold_notebook" {
-  path     = "/lucas_san20@hotmail.com/${var.notebook_subdirectory}/problema_gold.py"
+  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/problema_gold.py"
   language = var.notebook_language
   source   = "../app/src/problema_gold.py"
 }
 
 resource "databricks_notebook" "reclamacao_gold_notebook" {
-  path     = "/lucas_san20@hotmail.com/${var.notebook_subdirectory}/reclamacao_gold.py"
+  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/reclamacao_gold.py"
   language = var.notebook_language
   source   = "../app/src/reclamacao_gold.py"
 }
 
 resource "databricks_notebook" "resposta_gold_notebook" {
-  path     = "/lucas_san20@hotmail.com/${var.notebook_subdirectory}/resposta_gold.py"
+  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/resposta_gold.py"
   language = var.notebook_language
   source   = "../app/src/resposta_gold.py"
 }
 
 resource "databricks_notebook" "uf_gold_notebook" {
-  path     = "/lucas_san20@hotmail.com/${var.notebook_subdirectory}/uf_gold.py"
+  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/uf_gold.py"
   language = var.notebook_language
   source   = "../app/src/uf_gold.py"
 }
