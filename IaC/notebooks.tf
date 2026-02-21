6d58863cd07237e080e6135447fe1b234a4707a7
Besgam <lucas.sa20@gmail.com>
2026-02-20 22:44:08 -0300

    terraform git actions

--- IaC/notebooks.tf
+++ IaC/notebooks.tf
@@ -1,5 +1,5 @@
 # Obtém detalhes sobre o usuário
-data "databricks_current_user" "me" {}
+#data "databricks_current_user" "me" {}
 
 variable "notebook_subdirectory" {
   description = "Nome para diretório onde armazenar o notebook."
@@ -12,7 +12,7 @@ variable "notebook_language" {
 }
 
 resource "databricks_notebook" "bronze_notebook" {
-  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/bronze.py"
+  path     = "lucas_san20@hotmail.com/${var.notebook_subdirectory}/bronze.py"
   language = var.notebook_language
   source   = "../app/src/bronze.py"
 }
@@ -22,31 +22,31 @@ output "notebook_url" {
 }
 
 resource "databricks_notebook" "silver_notebook" {
-  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/silver.py"
+  path     = "lucas_san20@hotmail.com/${var.notebook_subdirectory}/silver.py"
   language = var.notebook_language
   source   = "../app/src/silver.py"
 }
 
 resource "databricks_notebook" "problema_gold_notebook" {
-  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/problema_gold.py"
+  path     = "lucas_san20@hotmail.com/${var.notebook_subdirectory}/problema_gold.py"
   language = var.notebook_language
   source   = "../app/src/problema_gold.py"
 }
 
 resource "databricks_notebook" "reclamacao_gold_notebook" {
-  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/reclamacao_gold.py"
+  path     = "lucas_san20@hotmail.com/${var.notebook_subdirectory}/reclamacao_gold.py"
   language = var.notebook_language
   source   = "../app/src/reclamacao_gold.py"
 }
 
 resource "databricks_notebook" "resposta_gold_notebook" {
-  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/resposta_gold.py"
+  path     = "lucas_san20@hotmail.com/${var.notebook_subdirectory}/resposta_gold.py"
   language = var.notebook_language
   source   = "../app/src/resposta_gold.py"
 }
 
 resource "databricks_notebook" "uf_gold_notebook" {
-  path     = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}/uf_gold.py"
+  path     = "lucas_san20@hotmail.com/${var.notebook_subdirectory}/uf_gold.py"
   language = var.notebook_language
   source   = "../app/src/uf_gold.py"
 }
