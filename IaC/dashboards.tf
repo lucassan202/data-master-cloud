234f5252a91f5f6a70820299a51f152aaa1a1d05
Besgam <lucas.sa20@gmail.com>
2026-03-15 17:54:46 -0300

    destroy

--- IaC/dashboards.tf
+++ IaC/dashboards.tf
@@ -5,7 +5,7 @@ data "databricks_sql_warehouses" "by_name" {
 
 resource "databricks_dashboard" "consumidor" {
   display_name         = "Consumidor"
-  warehouse_id         = data.databricks_sql_warehouses.by_name.ids[0]
+  warehouse_id         = tolist(data.databricks_sql_warehouses.by_name.ids)[0]
   serialized_dashboard = file("../dash/dash_consumidor.lvdash.json")
   parent_path          = "${data.databricks_current_user.me.home}/${var.notebook_subdirectory}"
   embed_credentials    = false
